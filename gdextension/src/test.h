b4685421df6d7296ee2e231af7a4a5c7190e440d
Camellia <56206613+sialiss@users.noreply.github.com>
2023-04-23 01:58:15 +0300

    Change something

---
+++ gdextension/src/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <godot_cpp/classes/node3d.hpp>
+
+using namespace godot:
+
+class Test : public Node3D {
+    GDCLASS(Test, Node3D);
+
+    protected:
+    static void _bind_methods(){}
+};
+
+#endif
